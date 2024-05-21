8b14ef2740bd51b0f6a0d5ccc587c03c0258179e
AmilaJayamaha <amilajayamaha96@gmail.com>
2024-05-21 21:00:47 +0530

    added some classes

---
+++ Menu/Menu/Data/MenuContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+
+namespace Menu.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext( DbContextOptions<MenuContext> options ) : base( options )
+        { 
+        }
+    }
+}
